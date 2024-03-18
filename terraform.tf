41f02bbbfe5cc436eb834be2b76e8a5db171d367
SyedYousufUddinHashmi <142165483+SyedYousufUddinHashmi@users.noreply.github.com>
2024-03-18 16:56:33 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,9 @@
+# Define a local file resource named "pet"
 resource "local_file" "pet" {
-filename = "/root/pets.txt"
-content = "we love pets!"
+  filename = "/root/pets.txt"
+  content  = "We love pets!"
 }
+
+# Run terraform apply with auto-approval
+terraform apply -auto-approve
+
