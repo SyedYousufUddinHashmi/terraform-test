1610fe40c64fe5d70e26c3f224aef0d6de043c88
SyedYousufUddinHashmi <142165483+SyedYousufUddinHashmi@users.noreply.github.com>
2024-03-25 12:38:36 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,15 @@
-# Define a local file resource named "pet"
-resource "local_file" "pet" {
-  filename = "/root/pets.txt"
-  content  = "We love pets!"
+resource "local_file" "My_pet" {
+    filename = "pet.txt"
+    content = "My cat name is ${random_pet.petname.id}"
+  
+}
+resource "random_pet" "petname" {
+    prefix = var.prefix
+    separator = "."
+    length = "1"
+  
+}
+output "petname" {
+   value = random_pet.petname.id
+  
 }
